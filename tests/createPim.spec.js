5b455728cb925370be51aa1ef9379df4e3e58ad1
kcconsigo <kcconsigo@gmail.com>
2025-04-03 13:01:21 +0800

    Modified: Update automated script and test data on these files

--- tests/createPim.spec.js
+++ tests/createPim.spec.js
@@ -4,21 +4,21 @@ const { PimPage } = require('../pages/pimpage.page');
 
 const credentials = JSON.parse(JSON.stringify(require("../utils/credentials.json")));
 const pimdetails = JSON.parse(JSON.stringify(require("../utils/pimdetails.json")));
-for (const pimdetailspage of pimdetails){
-    test(`should allow me to create Pim Employees ${credentials.validCredentials.username}, ${pimdetailspage.firstName}, ${pimdetailspage.middleName}, ${pimdetailspage.lastName}, ${pimdetailspage.empID}`, async ({ page }) => {
+  for (const pimdetailspage of pimdetails){
+  test(`should allow me to create Pim Employees ${credentials.validCredentials.username},${credentials.validCredentials.password}, ${pimdetailspage.firstName}, ${pimdetailspage.middleName}, ${pimdetailspage.lastName}, ${pimdetailspage.empID},${pimdetailspage.nationality[2]}, ${pimdetailspage.maritalstatus[1]}`, async ({ page }) => {
+    const loginpage = new LoginPage(page);
+    await loginpage.gotoLogin();
+    await loginpage.loginCredentials(credentials.validCredentials.username, credentials.validCredentials.password);
+    await loginpage.loginBtn();
 
-        const loginpage = new LoginPage(page);
-        await loginpage.gotoLogin();
-        await loginpage.loginCredentials(credentials.validCredentials.username, credentials.validCredentials.password);
-        await loginpage.loginBtn();
-    
-        const pimpage = new PimPage(page);
-        await pimpage.PimTab();
-        await pimpage.addEmpTab();
-        await pimpage.addEmployeeDetails(pimdetailspage.firstName, pimdetailspage.middleName, pimdetailspage.lastName, pimdetailspage.empID);
-        await pimpage.employeeListlandingTab(pimdetailspage.firstName);
-        await loginpage.logoutItemBtn();
-        await loginpage.logoutBtn();
-      });
+    const pimpage = new PimPage(page);
+    await pimpage.PimTab();
+    await pimpage.addEmpTab();
+    await pimpage.addEmployeeDetails(pimdetailspage.firstName, pimdetailspage.middleName, pimdetailspage.lastName, pimdetailspage.empID, pimdetailspage.nationality[2],pimdetailspage.maritalstatus[1]);
+    await pimpage.employeeListlandingTab(pimdetailspage.firstName);
+    await loginpage.logoutItemBtn();
+    await loginpage.logoutBtn();
+  });
 };
+
 
