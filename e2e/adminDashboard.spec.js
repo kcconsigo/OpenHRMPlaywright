76c313d0b26dd36920fdbfb3f889621f6c98402a
kcconsigo <kcconsigo@gmail.com>
2025-04-15 13:40:20 +0800

    modified: Update automated scripts, refactor the dynamic test data

--- e2e/adminDashboard.spec.js
+++ e2e/adminDashboard.spec.js
@@ -5,14 +5,14 @@ const { AdminDashboardPage } = require('../pages/admindashboardpage.page');
 const credentials = JSON.parse(JSON.stringify(require("../utils/credentials.json")));
 const admindashboarddetails = JSON.parse(JSON.stringify(require("../utils/admindashboarduser.json")));
 
-test(`should allow me to select Admin details ${credentials.validCredentials.username}, ${credentials.validCredentials.password}`, async ({ page }) => {
+test(`should allow me to select Admin details ${credentials.validCredentials.username}, ${credentials.validCredentials.password}, ${admindashboarddetails.Admins.UserName}`, async ({ page }) => {
     const loginpage = new LoginPage(page);
     await loginpage.gotoLogin();
     await loginpage.loginCredentials(credentials.validCredentials.username, credentials.validCredentials.password);
     await loginpage.loginBtn();
     const admindashboardpage = new AdminDashboardPage(page);
     await admindashboardpage.AdminTabPage();
-    await admindashboardpage.admindetailsRecords();
+    await admindashboardpage.admindetailsRecords(admindashboarddetails.Admins.UserName);
     await loginpage.logoutItemBtn();
     await loginpage.logoutBtn();
   });
--- pages/admindashboardpage.page.js
+++ pages/admindashboardpage.page.js
@@ -7,16 +7,19 @@ exports.AdminDashboardPage = class AdminDashboardPage{
    constructor(page){
       this.page = page;
       this.adminmenu = page.locator(".oxd-main-menu-item");
-      this.colCheckbox = page.locator('.oxd-table-cell.oxd-padding-cell');
    }
    async AdminTabPage(){
       await this.adminmenu.nth(0).click();
   }
-   async admindetailsRecords(){  
+   async admindetailsRecords(UserName){  
     await this.page.mouse.wheel(0, 100);        
     await this.page.mouse.move(20, 40);
-    await this.page.getByRole('row', { name: ' Dranreb Admin Robert James' }).locator('label').check();
-    await this.page.getByRole('row', { name: ' Yoo Nah Admin Ronaldo Gibbs' }).locator('label').check();
+   //  await this.page.getByRole('row', { name: UserName }).locator('label').check();
+   //  await this.page.getByRole('row', { name: UserName }).locator('label').check();
+    let rowList = this.page.locator("//div[@role='row']//following::div[@role='cell']//following::div//following::label//input[@type='checkbox']//ancestor::div[@role='row']//child::div[contains(text(), '"+UserName+"')]")
+    for(var x = 0; x < rowList;  x++){
+      await this.rowList.nth(x).click()
+    }
 
     let adminList = ['Dranreb', 'Yoo Nah']
     for(const admin of adminList ){
