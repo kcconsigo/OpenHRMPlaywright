b5c99c129b4bd1be800aa55cc1ebc11b1fd17007
kcconsigo <kcconsigo@gmail.com>
2025-04-17 16:32:46 +0800

    modified: tweaked automated scripts to organized the web locators accordingly

--- pages/adminpage.page.js
+++ pages/adminpage.page.js
@@ -1,4 +1,5 @@
 const { expect } = require('@playwright/test');
+import * as adminlocators from "../locators/adminpagelocators.json";
 
 exports.AdminPage = class AdminPage{
     /**
@@ -6,51 +7,27 @@ exports.AdminPage = class AdminPage{
     */
  constructor(page){
      this.page = page;
-     this.adminmenu = page.locator(".oxd-main-menu-item");
-     this.empAddButton = page.locator('.oxd-button');
-     this.userRoleSelectOptions = page.locator('.oxd-select-text');
      this.userRoleField = page.getByRole('option', { name: 'Admin' });
-     this.userStatus = page.locator('.oxd-select-text');
      this.userStatusField = page.getByRole('option', { name: 'Enabled' });
-     this.userEmpName = page.locator('.oxd-autocomplete-text-input');
-     this.userEmpNameSelect = page.locator('.oxd-autocomplete-dropdown');
-     this.username = page.locator('.oxd-input.oxd-input');
-     this.password = page.locator('.oxd-input.oxd-input');
-     this.comfirmPassword = page.locator('.oxd-input.oxd-input');
-     this.submitBtbnSave = page.locator('//button[@type="submit"]');
      this.successfullyMsg = page.getByText('SuccessSuccessfully Saved×');
-     this.adminList = page.locator('.oxd-table-body');
-     this.systemUser = page.locator('.oxd-input.oxd-input');
-     this.systemUserSearchbtn = page.locator('//button[normalize-space()="Search"]');
-     this.systemUserEditInfobtn = page.locator('//div[@class="orangehrm-paper-container"]//button[2]');
-     this.userRoleSelectOptionsEditInfo = page.locator('.oxd-select-text');
-     this.userRoleFieldEditInfo = page.locator('//div[@role="listbox"]//span[contains(text(),"ESS")]');
-     this.usernameEditInfo = page.locator('.oxd-input.oxd-input');
-     this.usernameEditInfoSavebtn = page.locator('//button[normalize-space()="Save"]');
      this.successfullyEditInfoMsg = page.getByText('SuccessSuccessfully Updated×');
-     this.scolldownInfo = page.locator('.orangehrm-container');
-     this.userDeleteInfo = page.locator('.oxd-checkbox-wrapper');
-     this.userDeleteDialogbox = page.locator('.oxd-sheet.oxd-sheet--rounded.oxd-sheet--white.oxd-dialog-sheet');
-     this.userDeletebtn = page.locator('//div[@class="orangehrm-container"]//button[1]');
-     this.userDeleteDialogbox = page.locator('.oxd-sheet.oxd-sheet--rounded.oxd-sheet--white.oxd-dialog-sheet');
-     this.userDeleteDialogbtn = page.locator('//button[normalize-space()="Yes, Delete"]');
      this.successfullyDeletedUser = page.getByText('SuccessSuccessfully Deleted×');
      this.NorecordsFound = page.getByText('InfoNo Records Found×');
    }
    async AdminTab(){
-     await this.adminmenu.nth(0).click();
-     await this.empAddButton.nth(2).click();
+    await this.page.locator(adminlocators.adminmenulocator.adminmenu).nth(0).click();
+    await this.page.locator(adminlocators.adminmenulocator.empAddButton).nth(2).click();
    }
    async selectUserRoleAndUserStatus(){
-     await this.userRoleSelectOptions.nth(0).click();
-     await this.userRoleField.click();
-     await this.userStatus.nth(1).click();
-     await this.userStatusField.click();
+    await this.page.locator(adminlocators.adminmenulocator.userRoleSelectOptions).nth(0).click();
+    await this.userRoleField.click();
+    await this.page.locator(adminlocators.adminmenulocator.userStatus).nth(1).click();
+    await this.userStatusField.click();
    }
  
    async createNewEmployeeInputTextFields(EmpName, UserName, Password, ConfirmPassword){
-     await this.userEmpName.getByPlaceholder('Type for hints...').fill(EmpName);
-     const EmpNameSelectList = this.userEmpNameSelect.getByRole('option', { name: EmpName });
+     await this.page.locator(adminlocators.adminmenulocator.userEmpName).getByPlaceholder('Type for hints...').fill(EmpName);
+    const EmpNameSelectList = this.page.locator(adminlocators.adminmenulocator.userEmpNameSelect).getByRole('option', { name: EmpName });
      for(let i = 0; i < EmpNameSelectList; ++i )
      {
        text = await this.userEmpNameSelect.nth(i).textContent();
@@ -61,14 +38,15 @@ exports.AdminPage = class AdminPage{
        }
        return EmpNameSelectList;
      }
-     await this.userEmpNameSelect.getByRole('option', { name: EmpName }).click();
-     await this.username.nth(1).fill(UserName);
-     await this.password.nth(2).fill(Password);
-     await this.comfirmPassword.nth(3).fill(ConfirmPassword);
+    //  await this.userEmpNameSelect.getByRole('option', { name: EmpName }).click();
+     await this.page.locator(adminlocators.adminmenulocator.userEmpNameSelect).getByRole('option', { name: EmpName }).click()
+     await this.page.locator(adminlocators.adminmenulocator.username).nth(1).fill(UserName);
+     await this.page.locator(adminlocators.adminmenulocator.password).nth(2).fill(Password);
+     await this.page.locator(adminlocators.adminmenulocator.comfirmPassword).nth(3).fill(ConfirmPassword);
    }
    async clickSavebtn(){
     await expect(async () => {
-        await this.submitBtbnSave.nth(0).click();
+        await this.page.locator(adminlocators.adminmenulocator.submitBtbnSave).nth(0).click();
         console.log(await this.successfullyMsg.textContent());
     }).toPass();
    }
